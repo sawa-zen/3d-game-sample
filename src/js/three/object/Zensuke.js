a911d9565f5665e749d3d8b5f99ccc62179f9ae7
sawada <sawadasuiren@gmail.com>
2017-01-11 14:18:56 +0900

    前進させるように修正

--- src/js/three/object/Zensuke.js
+++ src/js/three/object/Zensuke.js
@@ -15,6 +15,10 @@ export default class Zensuke extends THREE.Object3D {
 
     // アクション
     this._action = {};
+    // 速度
+    this._velocity = 0.3;
+    // 歩いているかどうか
+    this._isWalking = false;
 
     let loadResult = Loader.instance.getResult('zensuke');
     let jsonLoader = new THREE.JSONLoader();
@@ -65,6 +69,11 @@ export default class Zensuke extends THREE.Object3D {
     let delta = this._clock.getDelta();
     let theta = this._clock.getElapsedTime();
     this._mixer.update(delta);
+
+    // 歩いていれば前進させます。
+    if(this._isWalking) {
+      this.position.x += this._velocity;
+    }
   }
 
   /**
@@ -73,6 +82,8 @@ export default class Zensuke extends THREE.Object3D {
   walk() {
     // 歩きモーション開始
     this._action.walk.play();
+    // 歩いているフラグを立たせる
+    this._isWalking = true;
   }
 
   /**
@@ -80,5 +91,7 @@ export default class Zensuke extends THREE.Object3D {
    */
   idle() {
     this._action.walk.stop();
+    // 歩いているフラグを折る
+    this._isWalking = false;
   }
 }
